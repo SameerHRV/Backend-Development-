a6bf525e824884cd945a392a5ba2531caad6ddd0
SameerHRV <sameerharapanahalli2004@gmail.com>
2024-04-24 19:09:19 +0530

    add aggrie pipline

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -435,6 +435,76 @@ const updatedUserCoverImage = async (req, res) => {
   }
 }
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params
+
+  if (!username?.trim()) {
+    throw new createHttpError(400, 'Username is required')
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: 'subscriptions',
+        localField: '_id',
+        foreignField: 'channel',
+        as: 'subscribers',
+      },
+    },
+    {
+      $lookup: {
+        from: 'subscriptions',
+        localField: '_id',
+        foreignField: 'subscriber',
+        as: 'subscribedTo',
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: '$subscribers',
+        },
+        channelsSubscribedToCount: {
+          $size: '$subscribedTo',
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, '$subscribers.subscriber'] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ])
+
+  if (!channel?.length) {
+    throw new createHttpError(404, 'User channel profile not found')
+  }
+
+  return res.status(200).json(200, {
+    message: 'User channel profile fetched successfully',
+    channel: channel[0],
+  })
+})
+
 export {
   userRegister,
   loginUser,
@@ -445,4 +515,5 @@ export {
   getUserDetails,
   updatedUserAvatar,
   updatedUserCoverImage,
+  getUserChannelProfile,
 }
